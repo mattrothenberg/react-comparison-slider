8aafba072b33523aa9f020c2bb5a1abcda387f3a
Matt Rothenberg <mattrothenberg@github.com>
2021-07-20 17:14:33 -0400

    chore: enable Tailwind JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: [],
+  mode: 'jit',
+  purge: ['./stories/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -8,4 +9,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
